0d0ba8e8ee85186e7905634c2c07c3712bc92946
hamrohteam <162094767+hamrohteam@users.noreply.github.com>
2024-03-04 11:56:04 +0500

    Add files via upload

---
+++ 02.02-taklifnoma.py
@@ -0,0 +1,10 @@
+# Mavzu: Pythonda for operatori
+
+mehmonlar = ['shahrizoda','robiya','lola',
+             'soliha','ozoda','muslima',
+             'ramilya','abubakr','sardor',
+             'shahlo','mustafo','rayhona',
+             'ramilya','abubakr','sardor',
+             'shahlo','mustafo','rayhona',]
+for mehmon in mehmonlar:
+    print(mehmon,'sizni abubakrning toyiga aytishdi')
